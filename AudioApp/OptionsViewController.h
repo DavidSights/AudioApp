d2ffa5735c8d608ec36ea317adf10b73f7ead6ac
DavidSights <davidseitzjr@gmail.com>
2015-06-15 14:03:04 -0500

    Add UI elements to views, add an Options view controller.

---
+++ AudioApp/OptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.h
+//  AudioApp
+//
+//  Created by David Seitz Jr on 6/15/15.
+//  Copyright (c) 2015 DavidSights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UIViewController
+
+@end
---
+++ AudioApp/OptionsViewController.m
@@ -0,0 +1,26 @@
+//
+//  OptionsViewController.m
+//  AudioApp
+//
+//  Created by David Seitz Jr on 6/15/15.
+//  Copyright (c) 2015 DavidSights. All rights reserved.
+//
+
+#import "OptionsViewController.h"
+
+@interface OptionsViewController ()
+
+@end
+
+@implementation OptionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)logoutButtonPressed:(id)sender {
+
+    // Set up logout here.
+}
+
+@end
