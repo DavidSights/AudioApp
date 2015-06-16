b364124996f088c9cc2050de7f92ad17d63471e1
Alex <alexsantorineos@gmail.com>
2015-06-16 14:13:11 -0500

    parse

---
+++ AudioApp/PostViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostViewController.h
+//  AudioApp
+//
+//  Created by Alex Santorineos on 6/16/15.
+//  Copyright (c) 2015 DavidSights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+@interface PostViewController : UIViewController
+
+@property AVAudioRecorder *recorder;
+
+@end
