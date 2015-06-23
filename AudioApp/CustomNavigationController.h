89b643f22cac5d8f39c40afcc8fa613d15c3f5ec
DavidSights <davidseitzjr@gmail.com>
2015-06-23 18:24:43 -0500

    Set up custom navigation bar class. Fixed audio playing from wrong place bug.

---
+++ AudioApp/CustomNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CustomNavigationController.h
+//  AudioApp
+//
+//  Created by David Seitz Jr on 6/23/15.
+//  Copyright (c) 2015 DavidSights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomNavigationController : UINavigationController
+
+@end
