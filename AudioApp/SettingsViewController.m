5f57ddbcd687bcb446237226ea0b2393191a7317
Alex <alexsantorineos@gmail.com>
2015-06-26 11:10:15 -0500

    video

--- AudioApp/SettingsViewController.m
+++ AudioApp/SettingsViewController.m
@@ -9,12 +9,16 @@
 #import "SettingsViewController.h"
 #import "ProfileViewController.h"
 #import <Parse/Parse.h>
-@interface SettingsViewController ()<UITextFieldDelegate, UITextViewDelegate>
+@interface SettingsViewController ()<UITextFieldDelegate, UITextViewDelegate, UIImagePickerControllerDelegate, UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *username;
 @property (weak, nonatomic) IBOutlet UITextField *displaynameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextView *aboutTextView;
 @property PFUser *user;
+@property UIImage *image;
+@property (nonatomic,strong) UIImagePickerController *imagePicker;
+@property NSMutableArray *images;
+@property UIImage *nImage;
 @end
 
 @implementation SettingsViewController
@@ -75,6 +79,118 @@
  [self.username endEditing:YES];
 
  return YES;
+}
+
+- (IBAction)onEditPicButtonTapped:(id)sender {
+
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Add Profile Picture" message:@"Do you want to take a picture or upload a picture?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Upload", @"Take Picture", nil];
+
+    [alert show];
+}
+
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+
+    if (buttonIndex == 1) {
+        NSLog(@"Upload tapped");
+
+        [self uploadFromPhotoAlbum];
+    } else if (buttonIndex == 2) {
+
+        [self uploadFromCamera];
+    }
+    
+}
+
+
+
+-(void)uploadFromPhotoAlbum {
+
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    [self presentViewController:imagePicker animated:YES completion:nil];
+
+}
+
+-(void)uploadFromCamera {
+
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+
+    imagePicker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+
+    [self presentViewController:imagePicker animated:YES completion:nil];
+    
+}
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+
+    self.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+
+    [self uploadToParse];
+
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
++ (UIImage*)imageWithImage:(UIImage*)image
+              scaledToSize:(CGSize)newSize;
+{
+    UIGraphicsBeginImageContext( newSize );
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return newImage;
+}
+
+- (void)uploadToParse {
+    NSData *fileData;
+    NSString *fileName;
+
+    if (self.image != nil) {
+//                       UIImage *newImage =self.image;
+
+//        self.nImage = [SettingsViewController imageWithImage:self.image scaledToSize:CGSizeMake(15, 15)];
+        fileData = UIImageJPEGRepresentation(self.image, 0.5);
+        fileName = @"profileImage.jpg";
+    }
+
+    PFFile *file = [PFFile fileWithName:fileName data:fileData];
+    [file saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (error) {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"An error occurred!"
+                                                                message:@"Please try again."
+                                                               delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+        else {
+            PFUser *user = [PFUser currentUser];
+
+            [user setObject:file forKey:@"profileImage"];
+
+            [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (error) {
+                    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"An error occurred!"
+                                                                        message:@"Please try again."
+                                                                       delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [alertView show];
+                }
+                else {
+                    // Everything was successful!
+                    [self reset];
+                }
+            }];
+        }
+    }];
+}
+
+- (void)reset {
+    self.imagePicker = nil;
+
 }
 
 
