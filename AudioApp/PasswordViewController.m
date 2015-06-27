f0b1d62596790b18baf0275007daeb5b13ab51c7
Alex <alexsantorineos@gmail.com>
2015-06-26 20:34:56 -0500

    starting

--- AudioApp/PasswordViewController.m
+++ AudioApp/PasswordViewController.m
@@ -38,9 +38,15 @@
 
 - (void) saveButtonPressed:(id)sender {
     PFUser *currentUser = [PFUser currentUser];
-    if ([self.oldPasswordTextField.text isEqualToString:currentUser.password]) {
-        if ([self.nPasswordTextField.text isEqualToString:self.repeatNewPasswordTextField.text]) {
-            currentUser.password = self.repeatNewPasswordTextField.text;
+
+    NSString *oldPass=[self.oldPasswordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *newPass =[self.nPasswordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *repeatNewPass=[self.repeatNewPasswordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+
+    if ([oldPass isEqualToString:currentUser.password]) {
+        if ([newPass isEqualToString:repeatNewPass]) {
+            currentUser.password = repeatNewPass;
             [currentUser saveInBackground];
             NSLog(@"Successfully saved new password.");
             [self.navigationController popToRootViewControllerAnimated:YES];
@@ -50,19 +56,42 @@
     } else {
         NSLog(@"not old password");
     }
+
+
+    if (![self.nPasswordTextField.text isEqualToString:self.repeatNewPasswordTextField.text] && [self.oldPasswordTextField.text isEqualToString:currentUser.password] ) {
+
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Wrong" message:@"Not your current password and new passwords don't match" delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }
+   else if (![self.oldPasswordTextField.text isEqualToString:currentUser.password]) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Wrong" message:@"Not your current password" delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }
+    else if ([self.nPasswordTextField.text isEqualToString:self.repeatNewPasswordTextField.text]) {
+
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Wrong" message:@"New passwords don't match" delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }
+
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
-    NSInteger nextTag = textField.tag + 1;
-    // Try to find next responder
-    UIResponder* nextResponder = [textField.superview viewWithTag:nextTag];
-    if (nextResponder) {
-        // Found next responder, so set it.
-        [nextResponder becomeFirstResponder];
-    } else {
-        // Not found, so remove keyboard.
-        [textField resignFirstResponder];
-    }
-    return NO; // We do not want UITextField to insert line-breaks.}
+NSInteger nextTag = textField.tag + 1;
+// Try to find next responder
+UIResponder* nextResponder = [textField.superview viewWithTag:nextTag];
+if (nextResponder) {
+    // Found next responder, so set it.
+    [nextResponder becomeFirstResponder];
+} else {
+    // Not found, so remove keyboard.
+    [textField resignFirstResponder];
 }
+    return NO;
+
+}
+
+
 @end
