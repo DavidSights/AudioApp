de53ba1d33181fc467cade10164825314047bbbe
Alex <alexsantorineos@gmail.com>
2015-06-28 18:41:31 -0500

    activity

--- AudioApp/ActvityViewController.m
+++ AudioApp/ActvityViewController.m
@@ -5,35 +5,53 @@
 //  Created by Alex Santorineos on 6/25/15.
 //  Copyright (c) 2015 DavidSights. All rights reserved.
 //
-
+#import <Parse/Parse.h>
 #import "ActvityViewController.h"
 
 @interface ActvityViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *userImageView;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
-
+@property NSArray* activities;
 @end
 
 @implementation ActvityViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    PFQuery *activityQuery = [PFQuery queryWithClassName:@"Activity"];
+    [activityQuery whereKey:@"toUser" equalTo:[PFUser currentUser]];
+//    [commentsQuery includeKey:@"fromUser"];
+
+    [activityQuery orderByAscending:@"createdAt"];
+    [activityQuery findObjectsInBackgroundWithBlock:^(NSArray *activities, NSError *error) {
+        if (!error) {
+            self.activities = activities;
+            NSLog(@"%i", self.activities.count);
+            NSLog(@"%@",self.activities.lastObject);
+        }
+    }];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated{
+
+    PFQuery *activityQuery = [PFQuery queryWithClassName:@"Activity"];
+    [activityQuery whereKey:@"toUser" equalTo:[PFUser currentUser]];
+    //    [commentsQuery includeKey:@"fromUser"];
+
+    [activityQuery orderByAscending:@"createdAt"];
+    [activityQuery findObjectsInBackgroundWithBlock:^(NSArray *activities, NSError *error) {
+        if (!error) {
+            self.activities = activities;
+            NSLog(@"%i", self.activities.count);
+            NSLog(@"%@-------------",self.activities.lastObject);
+        }
+    }];
+
+
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
